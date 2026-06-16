180ffbfd2b4615c45f4fb27c2c6d7e1f255d2013
Michel Janssen <michel@rocaonline.nl>
2026-06-16 13:14:05 +0200

    added playwright by cron

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,27 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+  ? [
+      ['github'],
+      [
+        'html',
+        {
+          outputFolder: 'playwright-report',
+          open: 'never',
+        },
+      ],
+    ]
+  : [
+      [
+        'html',
+        {
+          outputFolder: 'playwright-report',
+          open: 'never',
+        },
+      ],
+    ],
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
